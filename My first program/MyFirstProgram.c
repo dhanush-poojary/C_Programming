5e9011f94028ca11f761b5e0633fbddbc8db63bf
Dhanush <legendarycoder6@gmail.com>
2026-08-25 12:53:50 +0530

    Updated FirstProgram File

---
+++ My first program/MyFirstProgram.c
@@ -0,0 +1,7 @@
+//my first c program
+#include<stdio.h>
+int main()
+{
+  printf("\nHello World!");
+  return 0;
+}
--- My first program/test1.c
+++
@@ -1,7 +0,0 @@
-//my first c program
-#include<stdio.h>
-int main()
-{
-  printf("\nHello canara.");
-  return 0;
-}
