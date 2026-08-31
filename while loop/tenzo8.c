56c0c946c3dd2856023313ee02911c53548ce01d
Dhanush <legendarycoder6@gmail.com>
2026-08-31 21:21:35 +0530

    Updated While loop file

--- while loop/tenzo8.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>  //BASIC INFINITE WHILE LOOP
-int main(){
-  int i=1; 
-  while(i==1){ //we didnt give any increament/decreament so the condition is always true
-    printf("HACKED!!");
-  }
-}
